5399163510e6220841c6cb0d7b976e0d27fbaa80
Ian-Acosta2330 <ian2330acosta@gmail.com>
2024-03-21 17:44:21 -0600

    Updatd history.c with adding history and printing it

--- src/history.c
+++ src/history.c
@@ -10,20 +10,39 @@ List* init_history() {
 
 void add_history(List *list, char *str) {
   Item *tempItem = (Item*)malloc(sizeof(Item));
+  Item *temp = list->root;
+  int id = 0;
+  tempItem->id = id;
+  tempItem->str = str;
   if(list->root == NULL) {
-    list->root = str;
+    printf("root is null\n");
+    list->root = tempItem;
+    tempItem->next = NULL;
+    // list->root = str;
+  } else {
+    while(temp->next != NULL) {
+      printf("Have yet to reach the end\n");
+      temp = temp->next;
+      id++;
+    }
+    temp->next = tempItem;
+    tempItem->next = NULL;
   }
-  list->tempItem = str;
 }
 
 char *get_history(List *list, int id) {
   // return list->id;
+  
 }
 
 void print_history(List *list) {
-  // while(list!=NULL) {
-    // printf("%s\n", list->str);
-    //}
+  printf("Calling print history\n");
+  while(!(list->root == NULL)) {
+    printf("\nIn the loop ");
+    printf("%s\n", list->root->str);
+    list->root = list->root->next;
+  }
+  
 }
 
 void free_history(List *list){
@@ -31,6 +50,13 @@ void free_history(List *list){
 }
 
 int main() {
-  // s_List *l;
-  //l = init_history();
+  //  Item *i1 = (Item*)malloc(sizeof(Item));
+  //  Item *i2 = (Item*)malloc(sizeof(Item));
+  List *l;
+  char *c = "abc"; 
+  l = init_history();
+  add_history(l,c);
+  char *c1 = "dbe";
+  add_history(l, c1);
+  print_history(l);
 }
